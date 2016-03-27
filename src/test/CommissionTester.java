9712e6991200432e280fd461346a3a03efcb83a3
Ahmad Husain <husaina@my.erau.edu>
2016-03-27 15:23:35 -0400

    Tested the calculate bonus commission method

--- src/test/CommissionTester.java
+++ src/test/CommissionTester.java
@@ -8,6 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import application.CommissionCalculator;
+import application.SalesTransaction;
 
 
 public class CommissionTester {
@@ -35,7 +36,7 @@ public class CommissionTester {
 		
 		// Create an instance of Commission Calculator 
 		CommissionCalculator c = new CommissionCalculator("Ahmad", 0);
-		CommissionCalculator c2 = new CommissionCalculator("Ahmad", 2);
+		CommissionCalculator c2 = new CommissionCalculator("John", 2);
 		
 		// Test getName()
 		assertEquals("Ahmad", c.getName());
@@ -64,4 +65,62 @@ public class CommissionTester {
 		assertEquals(200,c.getTotalSales(),0);
 	}
 	
+	// Test suite to test calculateBonusCommission()
+	@Test
+	public void BonusCommissionCalculatorTester(){
+		
+		// Create instances for PROBARIONARY type employers
+		CommissionCalculator p1 = new CommissionCalculator("Ahmad", 0);
+		CommissionCalculator p2 = new CommissionCalculator("Daniel", 0);
+		CommissionCalculator p3 = new CommissionCalculator("David", 0);
+		
+		// Create instances for EXPERIENCED type employers
+		CommissionCalculator e1 = new CommissionCalculator("John", 1);
+		CommissionCalculator e2 = new CommissionCalculator("Ahab", 1);
+		CommissionCalculator e3 = new CommissionCalculator("Ishmael", 1);
+		
+		// Create wrong type of experience employer
+		CommissionCalculator w = new CommissionCalculator("Miller", 3);
+		
+		// To check bonuses for PROBATIONARY type employers, three cases. 
+		// Amount exceeded threshold
+		p1.addSale(0, 30000);
+		p1.addSale(0, 30000);
+		assertEquals(60000, p1.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(50,p1.calculateBonusCommission(),0); // Assure bonus is correct
+		
+		// Amount equals threshold, no bonus :(, close though ;)
+		p2.addSale(0, 50000); 
+		assertEquals(50000, p2.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(0,p2.calculateBonusCommission(),0); // Assure bonus is correct
+		
+		// Amount less than threshold, no bonus :(
+		p3.addSale(0, 40000); 
+		assertEquals(40000, p3.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(0,p3.calculateBonusCommission(),0); // Assure bonus is correct
+		
+		// To check bonuses for EXPERIENCED type employers, three cases. 
+		// Amount exceeded threshold
+		e1.addSale(0, 60000);
+		e1.addSale(0, 60000); 
+		assertEquals(120000, e1.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(300,e1.calculateBonusCommission(),0); // Assure bonus is correct
+		
+		// Amount equals threshold, no bonus :(, close though ;)
+		e2.addSale(0, 100000.00); 
+		assertEquals(100000.00, e2.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(0,e2.calculateBonusCommission(),0); // Assure bonus is correct
+		
+		// Amount less than threshold, no bonus :(
+		e3.addSale(0, 90000); 
+		assertEquals(90000, e3.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(0,e3.calculateBonusCommission(),0); // Assure bonus is correct	
+		
+		// Check if method still works with wrong type of parameters
+		w.addSale(0, 90000); 
+		assertEquals(90000, w.getTotalSales(),0); // Assure total sales is correct
+		assertEquals(0,w.calculateBonusCommission(),0); // Assure bonus is correct	
+	}
 }
+
+	
